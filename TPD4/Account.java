99b0f44290c75a9940730831da57bca6a0e12cee
Joshua Keifer <35159057+KilyKilyPower@users.noreply.github.com>
2023-09-24 20:08:16 -0400

    Added some documentation for the Account class

--- TPD4/Account.java
+++ TPD4/Account.java
@@ -1,5 +1,16 @@
 import java.util.*;
 
+/**
+ * Account is the class for all accounts within the application. It requires a CofC ID 
+ * integer, a password, a valid email and a birthday. It will have a LinkedList to 
+ * contain all tickets purchased with the account. 
+ * <p></p>
+ * @author Joshua Keifer
+ * @version 1.0 24 September 2023
+ * <p></p>
+ *
+ **/
+
 class Account {
     private int cofcID;
     private String password;
@@ -13,6 +24,14 @@ class Account {
         tickets = new LinkedList<String>();
     }
 
+    /*
+     * @brief The overloaded account constructor
+     * @param cofcID: College of Charleston ID number, 
+     *        password: the user's password as a String,
+     *        email: the user's email
+     * @return n/a
+     */
+
     public Account(int cofcID, String password, String email) {
         this.cofcID = cofcID;
         this.password = password;
@@ -20,41 +39,91 @@ class Account {
         tickets = new LinkedList<String>();
     }
 
+    /*
+    * @brief Returns the user's ID
+    * @param n/a
+    * @return cofcID
+    */
+
     public int getCofcID() {
         return this.cofcID;
     }
+
+    /*
+     * @brief Returns the user's password
+     * @param n/a
+    * @return password
+    */
 
     public String getPassword() {
         return this.password;
     }
 
+    /*
+     * @brief Returns the user's email
+     * @param n/a
+     * @return email
+     */
+
     public String getEmail() {
         return this.email;
     }
+
+    /*
+     * @brief Resets the cofc ID
+     * @param cofcID: a new College of Charleston ID to override the old ID
+     * @return n/a
+     */
 
     public void setCofcID(int cofcID) {
         this.cofcID = cofcID;
     }
 
+    /*
+     * @brief Resets the user's password
+     * @param password: a new password for the user's account
+     * @return n/a
+     */
+
     public void setPassword(String password) {
         this.password = password;
     }
+
+    /*
+     * @brief Resets the user's email
+     * @param email: the new email for the user's account
+     * @return n/a
+     */
 
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /*
+     * @brief Adds a ticket the user's account
+     * @param ticket: will be a ticket object to be added, but is a string for now
+     * @return none
+     */
+
     public void setTickets(String ticket) {
         this.tickets.add(ticket);
     }
 
-    public void getTickets() {
+    /*
+     * @brief Prints tickets to the console and reutrns all tickets
+     * @param n/a
+     * @return tickets: a list of all tickets tied to the user
+     */
+
+    public LinkedList<String> getTickets() {
         this.setTickets("Noah Kahan");
         String[] ticketArray = new String[tickets.size()];
         for(int i = 0; i < ticketArray.length; i++) {
             ticketArray[i] = tickets.get(i);
             System.out.print(ticketArray[i] + " ");
         }
+
+        return tickets;
     }
 
     public static void main(String[] args) {
