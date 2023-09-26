82a256e8db8abf956238741820d0721874bb7c52
AnthonyRuvo <143139364+AnthonyRuvo@users.noreply.github.com>
2023-09-25 20:48:40 -0400

    Basic idea

---
+++ TPD4/Event.java
@@ -0,0 +1,17 @@
+import java.util.Date;
+/**
+ * Date is a class that contains the framework for an Event object. The event object contains information about
+ * the type of event, location, and the date of the event. Event uses java.util.Date to provide the
+ * information for the event's date.
+ *
+ * @author Anthony Ruvo
+ * @version 1.0 24 September 2023
+ * <p></p>
+ *
+ **/
+public class Event {
+    private String eventType;
+    private Date date;
+    private String location;
+
+}
