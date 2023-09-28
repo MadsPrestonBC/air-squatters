87417dd4693ee49c4cd3613e374b5fa6a378fbf7
AnthonyRuvo <143139364+AnthonyRuvo@users.noreply.github.com>
2023-09-28 12:29:45 -0400

    a few methods added

--- TPD4/TicketSystem.java
+++ TPD4/TicketSystem.java
@@ -1,7 +1,26 @@
+import java.util.LinkedList;
+
 public class TicketSystem {
 
+    private LinkedList<Ticket> cart;
+    private Account activeAccount;
+    boolean loggedIn;
 
-    public void logIn(int cofcID, String password) {
+    // TODO FILL STUFF OUT
+    public void checkUserPass(int cofcID, String password) {
+
+    }
+
+    // TODO FILL STUFF OUT
+    public boolean processPayment() {
+
+    }
+    // TODO FILL STUFF OUT
+    public void createReceipt() {
+
+    }
+    // TODO FILL STUFF OUT
+    public void generateTickets() {
 
     }
 
