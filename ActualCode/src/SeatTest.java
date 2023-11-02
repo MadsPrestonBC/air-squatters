3cb154df2b5e4d0b1d4ac269605fa8a23793bfbd
AnthonyRuvo <143139364+AnthonyRuvo@users.noreply.github.com>
2023-11-02 16:24:21 -0400

    SeatTest

--- ActualCode/src/SeatTest.java
+++ ActualCode/src/SeatTest.java
@@ -2,63 +2,36 @@ public class SeatTest {
 
     public static boolean returnTrueIfgetSeatWorks() {
         Seat testSeat = new Seat();
-
-        if (testSeat.getSeatNumber() == 0) {
-            return true;
-        } else {
-            return false;
-        }
+        return testSeat.getSeatNumber() == 0;
     }
 
     public static boolean returnTrueIfgetSectionWorks() {
         Seat testSeat = new Seat();
-        if (testSeat.getSection() == 'a') {
-            return true;
-        } else {
-            return false;
-        }
+        return testSeat.getSection() == 'a';
     }
 
     public static boolean returnTrueIfgetPriceWorks() {
         Seat testSeat = new Seat();
-        if (testSeat.getPrice() == 0.0) {
-            return true;
-        }
-        else {
-            return false;
-        }
+        return testSeat.getPrice() == 0.0;
     }
 
     public static boolean returnTrueIfsetSeatWorks() {
         Seat testSeat = new Seat();
         testSeat.setSeatNumber(10);
-        if (testSeat.getSeatNumber() == 10) {
-            return true;
-        } else {
-            return false;
-        }
+        return testSeat.getSeatNumber() == 10;
     }
 
 
     public static boolean returnTrueIfsetSectionWorks() {
         Seat testSeat = new Seat();
         testSeat.setSection('G');
-        if (testSeat.getSection() == 'G') {
-            return true;
-        } else {
-            return false;
-        }
+        return testSeat.getSection() == 'G';
     }
 
     public static boolean returnTrueIfsetPriceWorks() {
         Seat testSeat = new Seat();
         testSeat.setPrice(100.0);
-        if (testSeat.getPrice() == 100.0) {
-            return true;
-        }
-        else {
-            return false;
-        }
+        return testSeat.getPrice() == 100.0;
     }
 
     public static void main(String[] args) {
