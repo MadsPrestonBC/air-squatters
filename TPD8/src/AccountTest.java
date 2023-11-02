f71603722c420176d5b341ff29ffe04bd1f12be6
AnthonyRuvo <143139364+AnthonyRuvo@users.noreply.github.com>
2023-11-02 16:40:06 -0400

    TPD8 - Just look at Seat and Account, other classes were needed to make them work

--- TPD8/src/AccountTest.java
+++ TPD8/src/AccountTest.java
@@ -4,45 +4,28 @@ public class AccountTest {
         Account test = null;
         test = new Account(12345, "password", "clyde@cofc.edu");
 
-        if (test != null) {
-            return true;
-        }
-        else {
-            return false;
-        }
+        return test != null;
     }
 
     public static boolean returnTrueIfUserNameUpdated() {
         Account test = new Account(12345, "password", "clyde@cofc.edu");
         test.setCofcID(56789);
 
-        if (test.getCofcID() == 56789) {
-            return true;
-        } else {
-            return false;
-        }
+        return test.getCofcID() == 56789;
     }
 
         public static boolean returnTrueIfPasswordUpdated() {
             Account test = new Account(12345, "password", "clyde@cofc.edu");
             test.setPassword("NewPassword");
 
-            if (test.getPassword() == "NewPassword") {
-                return true;
-            } else {
-                return false;
-            }
+            return test.getPassword() == "NewPassword";
         }
 
     public static boolean returnTrueIfEmailUpdated() {
         Account test = new Account(12345, "password", "clyde@cofc.edu");
         test.setEmail("JohnDoe@cofc.edu");
 
-        if (test.getEmail() == "JohnDoe@cofc.edu") {
-            return true;
-        } else {
-            return false;
-        }
+        return test.getEmail() == "JohnDoe@cofc.edu";
     }
 
 
