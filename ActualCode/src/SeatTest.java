5df1a89b14f6f186bb982b252abbd48225a650b3
AnthonyRuvo <143139364+AnthonyRuvo@users.noreply.github.com>
2023-11-02 16:22:40 -0400

    SeatTest

--- ActualCode/src/SeatTest.java
+++ ActualCode/src/SeatTest.java
@@ -1,17 +1,78 @@
 public class SeatTest {
 
-    public static boolean returnTrueifGettersWork() {
+    public static boolean returnTrueIfgetSeatWorks() {
         Seat testSeat = new Seat();
 
         if (testSeat.getSeatNumber() == 0) {
             return true;
+        } else {
+            return false;
         }
+    }
+
+    public static boolean returnTrueIfgetSectionWorks() {
+        Seat testSeat = new Seat();
         if (testSeat.getSection() == 'a') {
             return true;
+        } else {
+            return false;
         }
+    }
+
+    public static boolean returnTrueIfgetPriceWorks() {
+        Seat testSeat = new Seat();
         if (testSeat.getPrice() == 0.0) {
             return true;
         }
+        else {
+            return false;
+        }
     }
+
+    public static boolean returnTrueIfsetSeatWorks() {
+        Seat testSeat = new Seat();
+        testSeat.setSeatNumber(10);
+        if (testSeat.getSeatNumber() == 10) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public static boolean returnTrueIfsetSectionWorks() {
+        Seat testSeat = new Seat();
+        testSeat.setSection('G');
+        if (testSeat.getSection() == 'G') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean returnTrueIfsetPriceWorks() {
+        Seat testSeat = new Seat();
+        testSeat.setPrice(100.0);
+        if (testSeat.getPrice() == 1000.0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(returnTrueIfgetSeatWorks());
+        System.out.println(returnTrueIfgetSectionWorks());
+        System.out.println(returnTrueIfgetPriceWorks());
+
+
+        System.out.println(returnTrueIfsetSeatWorks());
+        System.out.println(returnTrueIfsetSectionWorks());
+        System.out.println(returnTrueIfsetPriceWorks());
+
+
+    }
+
 
 }
