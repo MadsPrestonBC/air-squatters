aecee1c3654a3706a7151e89d1aaa1d1e6ec33cf
Joshua Keifer <lewlewkeifer@gmail.com>
2023-11-12 10:54:12 -0500

    Initialized Springboot

---
+++ app/TicketSystem/src/test/java/local/airsquatters/TicketSystem/TicketSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package local.airsquatters.TicketSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
